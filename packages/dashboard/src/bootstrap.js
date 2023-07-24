4abf988f1dedbe91053df4d107faa2e6aa22667a
harishatv <harisha.tv@powerschool.com>
2023-07-24 13:00:47 +0530

    dashboard changesa

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -18,5 +18,5 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // We are running through container
-// and we should export the mount function sasa
+// and we should export the mount function sasasas
 export { mount };
