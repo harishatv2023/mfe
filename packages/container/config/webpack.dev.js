45434a296fa99977205bf0570d6145a6d323a3eb
harishatv <harisha.tv@powerschool.com>
2023-07-24 12:23:34 +0530

    dashboard app

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,11 +1,12 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
-  output:{
-    publicPath:'http://localhost:8080/'
+  output: {
+    publicPath: 'http://localhost:8080/',
   },
   devServer: {
     port: 8080,
@@ -19,8 +20,9 @@ const devConfig = {
       remotes: {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js',
         auth: 'auth@http://localhost:8082/remoteEntry.js',
+        dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
       },
-      shared:['react','react-dom']
+      shared: packageJson.dependencies,
     }),
   ],
 };
